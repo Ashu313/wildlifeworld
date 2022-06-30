f07732311b82d3dfa8ce523b8129cb24b07a927d
Ashu313 <79858134+Ashu313@users.noreply.github.com>
2022-06-30 23:53:00 +0530

    add menu

--- src/common/Navbar/Navbar.js
+++ src/common/Navbar/Navbar.js
@@ -95,7 +95,62 @@ const Navbar=()=>{
        </div>
     
    </header>
+   <div className="container">
+<div className={click?"nav-menu1 active":"nav-menu1"}>
+       <div className="login-area">
+       <NavLink to="/Sign-in" ><i class="fa-solid fa-chevron-right"></i>Sign-in</NavLink>
+       </div>
+   
+       <div className="register-area">
+       <NavLink to="/Register" >
+       <i class="fa-solid fa-chevron-right"></i> Register</NavLink>
+       </div>
+      
+       </div>
 
+         
+   
+       
+   </div>*/
+
+   /*<div className="container-fluid flex_space ">
+
+<div className="logo">
+  <img src="https://wildlifesos.org/wp-content/uploads/2020/11/wsos-Bear-Logo-border-85x130-1.png" alt="animalphoto"></img>
+</div>
+
+<div className="contact-details flex_space">
+<div className="box flex_space">
+  <div className="icons">
+    <i className="fas fa-clock"></i>
+  </div>
+  <div className="text">
+    <h3>working hours</h3>
+    <NavLink to='/contact'>Monday to sunday :9.00am to 9:00pm</NavLink>
+  </div>
+</div>
+
+<div className="box flex_space">
+  <div className="icons">
+    <i className="fas fa-phone-volume"></i>
+  </div>
+  <div className="text">
+    <h3>Call Us</h3>
+    <NavLink to='/contact'>+01001010101010</NavLink>
+  </div>
+  </div>
+  <div className="box flex_space">
+  <div className="icons">
+    <i className="fas fa-envelope"></i>
+  </div>
+  <div className="text">
+    <h3>Mail Us</h3>
+    <NavLink to='/contact'>Kashutosh727@gmail.com</NavLink>
+  </div>
+</div>
+</div>
+
+</div>
 
    
 </>
