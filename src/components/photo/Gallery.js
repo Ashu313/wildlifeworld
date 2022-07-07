c9177ff36e088fff15a20010643be3456e209e01
Ashu313 <79858134+Ashu313@users.noreply.github.com>
2022-07-07 15:29:10 +0530

    footer change

--- src/components/photo/Gallery.js
+++ src/components/photo/Gallery.js
@@ -8,7 +8,7 @@ import HeadTitle from "../../common/Headtitle/Headtitle";
 const Gallery=()=>{
 return(
     
-   
+   <>
   <section>
     <HeadTitle></HeadTitle>
       <h1 class="heading">
@@ -33,8 +33,10 @@ return(
         </div>
         </div>
     </div>
-    <Footer></Footer>
+ 
    </section>
+      <Footer></Footer>
+      </>
 )
 
 }
